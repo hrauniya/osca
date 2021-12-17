41583f1e80364cd8d33ae8fa6bf37bf058b361ed
JHSieber <hsieber@oberlin.edu>
2021-12-17 13:56:56 -0500

    added host url to allowed hosts

--- osca/settings.py
+++ osca/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['agile-temple-00335.herokuapp.com']
 
 
 # Application definition
