bbbea0d5f56b501592557415f3b5dd6081c30c2d
JHSieber <hsieber@oberlin.edu>
2021-12-17 14:05:57 -0500

    reformatted urls

--- osca/urls.py
+++ osca/urls.py
@@ -1,7 +1,8 @@
-"""osca URL Configuration
 
+   
+"""osca URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,18 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-# Use include() to add paths from the catalog application
+from django.contrib import admin
+
+# Use include() to add URLS from the catalog application and authentication system
 from django.urls import include
-#Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
