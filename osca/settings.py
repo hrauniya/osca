17650aa5fddbd0edefd4e4fcef33b3804bec5756
N Plotkin <nplotkin@oberlin.edu>
2022-01-10 19:34:49 -0500

    Changing settings for serving static files

--- osca/settings.py
+++ osca/settings.py
@@ -45,8 +45,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # Set hosts to allow any app on Heroku and the local testing URL
 ALLOWED_HOSTS = ['agile-temple-00335.herokuapp.com','desolate-river-20580.herokuapp.com','127.0.0.1']
